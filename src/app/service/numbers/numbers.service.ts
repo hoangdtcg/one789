137456f304e01c2a54ea37baed371f18c2d259d1
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-08-21 13:54:03 +0700

    add getThreeDifferentNumber function

--- src/app/service/numbers/numbers.service.ts
+++ src/app/service/numbers/numbers.service.ts
@@ -59,4 +59,20 @@ export class NumbersService {
     }
     return result;
   }
+
+  getThreeDifferentNumber(array) {
+    let result = [];
+    for (let i = 0; i < array.length; i++) {
+      for (let j = i + 1; j < array.length; j++) {
+        for (let k = j + 1; k < array.length; k++) {
+          let arrayTemp = [];
+          arrayTemp.push(array[i]);
+          arrayTemp.push(array[j]);
+          arrayTemp.push(array[k]);
+          result.push(arrayTemp);
+        }
+      }
+    }
+    return result;
+  }
 }
