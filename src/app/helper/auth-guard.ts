76035df526eb4f84048d545f3647ce34fd190828
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-08-26 21:49:12 +0700

    add new auth guard

--- src/app/helper/auth-guard.ts
+++ src/app/helper/auth-guard.ts
@@ -11,24 +11,35 @@ import {
 } from '@angular/router';
 import {UserToken} from '../interface/user-token';
 import {AuthenticationService} from '../service/authentication/authentication.service';
+import {UserService} from '../service/user/user.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate, CanActivateChild, CanLoad {
   currentUser: UserToken;
+  isValid: boolean = false;
 
   constructor(private router: Router,
-              private authService: AuthenticationService) {
+              private authService: AuthenticationService,
+              private userService: UserService) {
     this.authService.currentUser.subscribe(
       user => {
         this.currentUser = user;
+        this.userService.getAllUser().subscribe(listUser => {
+          listUser.map(user1 => {
+            // @ts-ignore
+            if (this.currentUser.Username == user1.payload.doc.data().username) {
+              this.isValid = true;
+            }
+          });
+        });
       }
     );
   }
 
   canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    if (this.currentUser) {
+    if (this.isValid) {
       return true;
     } else {
       // not logged in so redirect to login page with the return url
@@ -38,7 +49,7 @@ export class AuthGuard implements CanActivate, CanActivateChild, CanLoad {
   }
 
   canActivateChild(next: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    if (this.currentUser) {
+    if (this.isValid) {
       return true;
     } else {
       // not logged in so redirect to login page with the return url
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,6 +6,7 @@ import {ActivatedRoute, Router} from '@angular/router';
 import {AuthenticationService} from '../service/authentication/authentication.service';
 import {NotificationService} from '../service/notification/notification.service';
 import {User} from '../interface/user';
+import {UserService} from '../service/user/user.service';
 
 @Component({
   selector: 'app-login',
@@ -23,7 +24,8 @@ export class LoginComponent implements OnInit {
   constructor(private activatedRoute: ActivatedRoute,
               private router: Router,
               private authenticationService: AuthenticationService,
-              private notificationService: NotificationService) {
+              private notificationService: NotificationService,
+              private userService: UserService) {
     this.authenticationService.currentUser.subscribe(value => this.currentUser = value);
     if (this.authenticationService.currentUserValue) {
       this.router.navigate(['/']);
@@ -35,18 +37,35 @@ export class LoginComponent implements OnInit {
   }
 
   login() {
-    this.authenticationService.login(this.loginRequest.Username, this.loginRequest.Password)
-      .pipe(first())
-      .subscribe(
-        data => {
-          sessionStorage.setItem('ACCESS_TOKEN', data.IdToken);
-          localStorage.setItem('username', this.loginRequest.Username);
-          this.router.navigate([this.returnUrl]).finally(() => {
+    if (this.isExistsInFirestore(this.loginRequest.Username)) {
+      this.authenticationService.login(this.loginRequest.Username, this.loginRequest.Password)
+        .pipe(first())
+        .subscribe(
+          data => {
+            sessionStorage.setItem('ACCESS_TOKEN', data.IdToken);
+            localStorage.setItem('username', this.loginRequest.Username);
+            this.router.navigate([this.returnUrl]).finally(() => {
+            });
+            this.notificationService.showSuccessMessage('Đăng nhập thành công!');
+          },
+          () => {
+            this.notificationService.showErrorMessage('Sai tên đăng nhập hoặc mật khẩu!');
           });
-          this.notificationService.showSuccessMessage('Đăng nhập thành công');
-        },
-        () => {
-          this.notificationService.showErrorMessage('Đăng nhập thất bại');
-        });
+    } else {
+      this.notificationService.showErrorMessage('Sai tên đăng nhập hoặc mật khẩu!');
+    }
+  }
+
+  isExistsInFirestore(username) {
+    let isExisted = false;
+    this.userService.getAllUser().subscribe(listUser => {
+      listUser.map(user => {
+        // @ts-ignore
+        if (user.payload.doc.data().username == username) {
+          isExisted = true;
+        }
+      });
+    });
+    return isExisted;
   }
 }
